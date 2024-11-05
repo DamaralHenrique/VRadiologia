51af141aeafeee570c1a559ebc16b8596a710a3d
DamaralHenrique <rique01101999@gmail.com>
2024-11-05 17:30:37 -0300

    Remove coment[arios

--- Assets/Scripts/Interactor.cs
+++ Assets/Scripts/Interactor.cs
@@ -17,43 +17,15 @@ public class Interactor : MonoBehaviour
     {
          highlightMaterial = Resources.Load<Material>("Outline");
 
-        // Verifique se o material foi carregado corretamente
         if (highlightMaterial == null)
         {
             Debug.LogError("Highlight Material não encontrado na pasta Resources!");
         }
-        // // Verifica se highlightMaterial foi arrastado; se não, cria um novo material com o shader de highlight
-        // if (highlightMaterial == null)
-        // {
-        //     // Substitua "HighlightShaderName" pelo nome exato do shader de highlight que você criou/importou
-        //     Material highlightMaterial = Material.Find("Outline");
-        //     if (highlightMaterial == null)
-        //     {
-        //         Debug.LogWarning("Material de highlight não encontrado!");
-        //     }
-        // }
     }
 
     // Update is called once per frame
     void Update()
     {
-        // if (Input.GetKeyDown(KeyCode.P))
-        // {
-        //     Ray r = new Ray(interactorSource.position, interactorSource.forward);
-        //     // Debug.DrawLine(r.origin, new Vector3(transform.position.x, transform.position.y, transform.rotation.z + interactRange));
-        //     Debug.DrawLine(r.origin, r.origin + r.direction * interactRange, Color.red);
-
-        //     if (Physics.Raycast(r, out RaycastHit hitInfo))
-        //     //if (Physics.Raycast(r, out RaycastHit hitInfo, interactRange))
-        //     {
-        //         Debug.Log(1);
-
-        //         if (hitInfo.collider.gameObject.TryGetComponent(out IInteractable interactObj)) {
-        //             interactObj.Interact();
-        //         }
-        //     }
-        // }
-
         Ray r = new Ray(interactorSource.position, interactorSource.forward);
         Debug.DrawLine(r.origin, r.origin + r.direction * interactRange, Color.red);
 
@@ -61,18 +33,15 @@ public class Interactor : MonoBehaviour
         {
             if (Input.GetKeyDown(KeyCode.P)){
                 Debug.Log(1);
-                Debug.Log("P!");
 
                 if (hitInfo.collider.gameObject.TryGetComponent(out IInteractable interactObj)) {
                     interactObj.Interact();
                 }
             }
             
-            // Debug.Log("name:  " + hitInfo.collider.gameObject.name);
-            // Debug.Log("renderer:  " + hitInfo.collider.gameObject.TryGetComponent(out Renderer renderer2));
             if (hitInfo.collider.gameObject.TryGetComponent(out Renderer renderer))
             {
-                // Debug.Log("entrou!");
+                Debug.Log("entrou!");
                 // Se o último renderer não é o mesmo do atual, restaure os materiais anteriores
                 if (lastRenderer != null && lastRenderer != renderer)
                 {
